e458b2cdead833cfa9da45a8160caee4ced4539d
Linxiao42 <linxiao_xu@126.com>
2021-05-19 15:42:03 +0800

    补充关联式容器示例

--- src/stl/set/自定义关联式容器排序规则.cpp
+++ src/stl/set/自定义关联式容器排序规则.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <set>
 #include <string>
+#include <set>
 
 using namespace std;
 
@@ -17,11 +17,13 @@ public:
 };
 
 // 定义函数对象模板类
-template <typename T>
-class cmp2 {
+template<typename T>
+class cmp2
+{
 public:
     // 重载 () 运算符
-    bool operator()(const T &a, const T &b) {
+    bool operator()(const T &a, const T &b)
+    {
         // 按照值的大小，做升序排序
         // 注意，此方式必须保证 T 类型元素可以直接使用关系运算符（比如这里的 < 运算符）做比较
         return a < b;
@@ -45,7 +47,57 @@ void functionObject()
     cout << endl;
 }
 
+/**
+ * 在STL标准库中，存在如下排序规则
+ * std::less<T> 底层采用 < 运算符实现升序排序，各关联式容器默认采用的排序规则
+ * std::greater<T> 底层采用 > 运算符实现降序排序，同样适用于各个关联式容器
+ * std::less_equal<T> 底层采用 <= 运算符实现升序排序，多用于 multimap 和 multiset 容器
+ * std::greater_equal<T> 底层采用 >= 运算符实现降序排序，多用于 multimap 和 multiset 容器
+ *
+ * 当容器中存储的数据类型为自定义结构体变量或者类对象时，需要重载关系运算符，才能使排序规则发挥作用
+ */
+class MyString
+{
+public:
+    explicit MyString(string s) : str(s)
+    {}
+
+    string getStr() const
+    {
+        return this->str;
+    }
+
+    bool operator<(const MyString &other) const
+    {
+        return this->str.length() < other.str.length();
+    }
+
+private:
+    string str;
+};
+
+/**
+ * 重载关系运算符实现自定义排序规则
+ */
+void relationOperator()
+{
+    // 创建空的set容器，仍使用默认的less<T>排序规则
+    set<MyString> mySet;
+
+    // 向set容器添加元素，这里会调用MyString类的构造函数
+    mySet.emplace("ab");
+    mySet.emplace("a");
+    mySet.emplace("abc");
+
+    for (auto iter = mySet.begin(); iter != mySet.end(); iter++) {
+        MyString myStr = *iter;
+        cout << myStr.getStr() << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     functionObject();
+    relationOperator();
 }
